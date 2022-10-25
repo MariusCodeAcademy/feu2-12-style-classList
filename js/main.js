fabd8201913c5a273cc01e9d0095ffecd854d856
Marius <marius@example.com>
2022-10-25 08:44:04 +0300

    uzd

--- js/main.js
+++ js/main.js
@@ -12,3 +12,10 @@ function makeStyleBig(element) {
 makeStyleBig(sectionTitleEl);
 
 //  <h4 id="autor">James Band</h4> pritaikyti makeStyleBig stiliu
+
+// nusittaikyti i paragrafa su tekstu, uzdedi linijos auksti 1.5 ir padidinti tarpa tarp raidziu keletu pixeliu, padaryti teksta pasvirusi i desine(italic)
+
+// <input type="text" id="artColorInput" placeholder="Article spalva">
+// ivedus spalva ta spalva turi nusidazyti article fonas. be mygtuko
+
+//  <button>make article 50% width</button> paspaudus sumazina article ploti iki 50%
