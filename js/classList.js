d5bb5f24694d561f692944c7cdd7654bbba4d086
Marius <marius@example.com>
2022-10-25 10:46:14 +0300

    classlist

---
+++ js/classList.js
@@ -0,0 +1,18 @@
+'use strict';
+console.log('classList.js');
+
+// element.classList    API
+// .add - prideti
+// .remove - pasalinti
+// .toggle - toglinti
+// .contains - grazina ar yra tokia klase
+
+// 1. uzdeti <main> klase 'container'
+
+// 2. <ul class="unlisted"> uzdeti klase 'card'
+
+// 3. uzdeti h1 elementui klase 'mainTitle' aprasyti css faile keleta taisyklius
+
+// 4. paspaudus ant h1 mes ijungiam arba isjungiam 'mainTitle' klase (toggle)
+
+// 5. atvaizduoti su alertu ar <ul class="unlisted"> turi klase unlisted (.contains)
