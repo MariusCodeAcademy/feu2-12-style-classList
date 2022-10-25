f234438f7e60403f6b9c029551b75a56660f5331
Marius <marius@example.com>
2022-10-25 11:24:59 +0300

    classLIst

--- js/classList.js
+++ js/classList.js
@@ -3,6 +3,8 @@ console.log('classList.js');
 
 const htmlEls = {
   main: document.querySelector('main'),
+  ul: document.querySelector('ul'),
+  h1: document.querySelector('h1'),
 };
 
 // classList    API
@@ -15,9 +17,12 @@ const htmlEls = {
 // 1. uzdeti <main> klase 'container'
 htmlEls.main.classList.add('container');
 // 2. <ul class="unlisted"> uzdeti klase 'card'
-
+htmlEls.ul.classList.add('card');
 // 3. uzdeti h1 elementui klase 'mainTitle' aprasyti css faile keleta taisyklius
-
+htmlEls.h1.classList.add('mainTitle');
 // 4. paspaudus ant h1 mes ijungiam arba isjungiam 'mainTitle' klase (toggle)
+htmlEls.h1.addEventListener('click', () => htmlEls.h1.classList.toggle('mainTitle'));
 
 // 5. atvaizduoti su alertu ar <ul class="unlisted"> turi klase unlisted (.contains)
+const isThereAClass = htmlEls.ul.classList.contains('unlisted');
+console.log('isThereAClass ===', isThereAClass);
