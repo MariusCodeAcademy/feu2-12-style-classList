e6fc122a3e0cf3b386797cf07b0619a6f8b2b302
Marius <marius@example.com>
2022-10-25 08:36:54 +0300

    makeStyleBig

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 'use strict';
 console.log('main.js');
+
+const sectionTitleEl = document.getElementById('sectionTitle');
+
+function makeStyleBig(element) {
+  element.style.fontWeight = 'normal';
+  element.style.fontWeight = 400;
+  element.style.fontSize = '3rem';
+  element.style.marginBottom = '0.3em';
+}
+makeStyleBig(sectionTitleEl);
+
+//  <h4 id="autor">James Band</h4> pritaikyti makeStyleBig stiliu
