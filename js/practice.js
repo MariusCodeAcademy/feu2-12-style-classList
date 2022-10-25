7df69182a3dde45d1615ed3fc5b2a74f06e4b0b3
Marius <marius@example.com>
2022-10-25 16:06:29 +0300

    local storage

--- js/practice.js
+++ js/practice.js
@@ -3,3 +3,28 @@ console.log('practice.js');
 
 const colorsArr = ['Red', 'Green', 'Blue', 'Violet'];
 const navItems = ['index', 'about', 'practice'];
+
+const btn1El = document.getElementById('btn1');
+const titleEl = document.getElementById('title');
+
+// paiimti reiksme ir ja panaudoti
+const savedTitleFromB4 = localStorage.getItem('docTitle');
+console.log('savedTitleFromB4 ===', savedTitleFromB4);
+if (savedTitleFromB4 !== null) {
+  document.title = savedTitleFromB4;
+}
+
+btn1El.addEventListener('click', () => {
+  console.log('click');
+  const newDocTitle = titleEl.value.trim();
+  document.title = newDocTitle;
+  // irasyti ir i local storage
+  localStorage.setItem('docTitle', newDocTitle);
+});
+
+// irasau i localStorage
+// localStorage.setItem('userName', 'James Bond');
+
+// pasiimu is localStorage
+// const usr = localStorage.getItem('userName');
+// console.log('usr ===', usr);
