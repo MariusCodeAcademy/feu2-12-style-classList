f959a512e7fb742142e659988fee239f808ec48f
Marius <marius@example.com>
2022-10-26 08:24:32 +0300

    current target

--- js/event.js
+++ js/event.js
@@ -6,16 +6,17 @@ const h1El = document.querySelector('h1');
 // event handleris yra aprasytas vietoje su arrow fn
 h1El.addEventListener('click', (event) => {
   // debugger;
-  console.log(event);
-  console.log('click1', day);
+  console.log(event.target);
+  console.log('click1');
 });
 
 // event handleris yra atskirai aprasytoje fn
 
-h1El.addEventListener('click', h1ClickHandler);
+// h1El.addEventListener('click', h1ClickHandler);
+document.body.addEventListener('click', h1ClickHandler);
 
 function h1ClickHandler(event) {
-  console.log(event);
+  console.log(event.currentTarget); // tas elementas kuriam uzdetas event listeneris
   console.log('click2');
 }
 
